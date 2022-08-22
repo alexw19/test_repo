5187af0b770128ef8e2bae09af6dcdfb1b4dcbcb
Alex <alex.wang19@gmail.com>
2022-08-21 22:28:32 -0500

    Update app.py
    add debug options

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ from endpoints.contact import contact_bp
 app = Flask(__name__)
 app.register_blueprint(home_bp, url_prefix='/home')
 app.register_blueprint(contact_bp, url_prefix='/contact')
-app.run()
+app.run(debug=True)
